6f0b28039e488e8d616caf406cc24d5983da6c48
hey-its-Sohan <its.sohan.dev@gmail.com>
2025-07-23 12:25:28 +0600

    initialized routes and authproviders

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+import Login from "../Pages/Login";
+import SignUp from "../Pages/SignUp";
+import Home from "../Pages/Home";
+import Root from "../Pages/Root";
+import Error from "../Pages/Error";
 
-const Routes = () => {
-  return (
-    <div>
 
-    </div>
-  );
-};
 
-export default Routes;
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <Error />,
+
+    children: [
+      {
+        path: "/",
+        Component: Home,
+      },
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: '/sign-up',
+        Component: SignUp,
+      },
+    ]
+  },
+]);
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AuthProvider from './Contexts/AuthProvider.jsx'
+import { RouterProvider } from 'react-router'
+import { ToastContainer } from 'react-toastify'
+import { router } from './Routes/Routes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+    <ToastContainer />
+
   </StrictMode>,
 )
